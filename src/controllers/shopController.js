382cb776e03757de418e94e0708e08dc3ef552d4
jcentenaro <info@pitzil.com>
2024-03-04 11:41:22 -0300

    Vista Shop
    Con error en vista ITEM

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,9 +1,35 @@
-const shopView = (req, res) => {
-    res.send("Shop HOME");
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
+
+const {validationResult } = require("express-validator");
+
+const model = require("../models/productos");
+
+const shopView = async (req, res) => {
+    try {
+      const productos = await model.findAll();
+      // console.log(productos);
+      res.render("shop/shop", { productos });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
 };
-const idView = (req, res) => {
-    res.send("Shop ITEM ID");
+const idView = async (req, res) => {
+    try {
+      const producto = await model.findByPk(req.params.id);
+  
+      if(producto) {
+        res.render("shop/item", { values: producto });
+      } else {
+        res.status(404).send("El Producto no existe");
+      }
+    } catch (error) {
+      console.log(error);
+    }  
 };
+ 
 const itemView = (req, res) => {
     res.send("Shop ITEM ID ADD");
 };
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,6 +1,25 @@
-const express = require("express");
+const {Router} = require("express");
 // no hago lo mismo que en app.js sino estaría replicando el server, aca solicito un módulo nativa de express para manejar estas rutas
-const router = express.Router();
+const router = Router();
+
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
+
+const { body } = require("express-validator");
+
+const validations = [
+    body("nombre")
+        .not()
+        .isEmpty()
+        .withMessage("El nombre es obligatorio")
+        .bail()
+        .isLength({ min: 3 })
+        .withMessage("Debe tener como mínimo 3 caracteres"),
+    body("precio")
+        .not()
+        .isEmpty()
+        .withMessage("El precio es obligatorio"),
+];
 
 const controller = require("../controllers/shopController");
 //cambio app por router que es lo que maneja las rutas
