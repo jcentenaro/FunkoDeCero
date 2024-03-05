13cede2d1dce4bcc0f21f6d3505635414e1804f7
jcentenaro <info@pitzil.com>
2024-03-05 13:15:48 -0300

    Vista de Item
    Se ve el producto sin los datos de BD

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,3 +1,5 @@
+
+const data = require("../services/service_product");
 const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
@@ -16,23 +18,28 @@ const shopView = async (req, res) => {
     res.status(500).send(error);
   }
 };
-const idView = async (req, res) => {
-  try {
-    const products = await model.findByPk(req.params.id);
 
-    if (product) {
-      res.render("shop/item", { values: products });
-    } else {
-      res.status(404).send("El Producto no existe");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+// const idView = async (req, res) => {
+//   const products = await data.getAllProducts();
+//   const id = req.params.id;
+//   const productById = await data.getProductById(id);
+//   console.log(productById);
+//   res.render("shop/item", {
+//     view: {
+//       title: "Item | Funkoshop"
+//     },
+//     products: products.data,
+//     productById: productById.data
+// })};
+
+const idView = (req, res) => {
+  res.render("shop/item");
 };
 
 const itemView = (req, res) => {
   res.send("Shop ITEM ID ADD");
 };
+
 const cartView = (req, res) => {
   res.send("Cart GET");
 };
