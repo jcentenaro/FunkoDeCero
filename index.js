90d00db90e9793e761eee8fa77c44cf2854e32ca
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2025-02-15 11:06:59 -0300

    varios

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.use((req, res, next) => {
     //res.status(404).render("404");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, async () => {
     try {
